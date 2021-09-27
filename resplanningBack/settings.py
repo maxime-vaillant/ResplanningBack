529f67a73d0d4ad7e4932bb84a7279189886dadf
“Maxime <max.vaillant1@orange.fr>
2021-09-27 18:56:29 +0200

    Static fix

--- resplanningBack/settings.py
+++ resplanningBack/settings.py
@@ -141,8 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
